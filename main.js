127887808c8efe25309130fca82fda5a1e1e1a81
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-18 08:00:07 -0300

    kj

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function TocaSomPom (){
-    document. querySelector ( 'som_tecla_pom') .play ( );
+    document.querySelector('#som-tecla-toim').play();
 }
 const Listadeteclas = document.querySelectorAll('.tecla'); 
 
  let contador = 0; 
 
-while(contador<9) {                                   
-    Listadeteclas[0] .onclick = TocaSomPom;
+while(contador < 9) {                                   
+    Listadeteclas[0].onclick = TocaSomPom;
 
      contador = contador + 1;
 console.log(contador);
