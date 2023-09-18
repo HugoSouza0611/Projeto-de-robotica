02ae4bb1712abd99be89c5dd99a480e90991a617
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-18 08:21:40 -0300

    p

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const Listadeteclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < 9) {
-    Listadeteclas[0].onclick = function () tocar ( '#som-tecla-toim')
+    Listadeteclas[0].onclick = function () tocar('#som-tecla-toim')
     contador = contador + 1;
     console.log(contador);
 }
