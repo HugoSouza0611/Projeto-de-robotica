c243c61fe4651aa1eae12fd061c035a7c64bb628
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-18 08:39:13 -0300

    dvb

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function TocaSomPom() {
 const Listadeteclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-
+const lista = Listadeteclas[contador].classList[1];
+console.log(lista);
 while (contador < 9) {
     Listadeteclas[0].onclick = function () tocar('#som-tecla-toim')
     contador = contador + 1;
