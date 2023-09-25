bcfba7291669f1a6a20985b6d58e0b62356c896e
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-25 07:30:07 -0300

    dfdg

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let contador = 0;
 const lista = Listadeteclas[contador].classList[1];
 console.log(lista);
 while (contador < 9) {
-    Listadeteclas[0].onclick = function () tocar('#som-tecla-toim')
+    Listadeteclas[0].onclick = function () {tocar('#som-tecla-toim');
     contador = contador + 1;
     console.log(contador);
 }
