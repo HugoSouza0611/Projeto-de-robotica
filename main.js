e738dfd42a7533abd91f83137d4045e20a432bc8
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-25 08:10:57 -0300

    jhdbgsbuysg

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ const Listadeteclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (Contador < Listadeteclas.length)  {
-
     Listadeteclas[Contador].onclick = function ()   {
-
         tocar(idAudio);
     };    
 const lista = Listadeteclas[contador].classList[1];
