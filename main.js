f873d310f2440a6acc00c360989d1a82de32d67e
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2023-09-25 07:56:06 -0300

    ^gjyklçntr6jiu6gkop

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ function TocaSomPom() {
 const Listadeteclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
+
+while (Contador < Listadeteclas.length)  {
+
+    Listadeteclas[Contador].onclick = function ()   {
+
+        tocar(idAudio);
+    };    
 const lista = Listadeteclas[contador].classList[1];
 console.log(lista);
-while (contador < 9) {
-    Listadeteclas[0].onclick = function () {tocar('#som-tecla-toim');
-    contador = contador + 1;
-    console.log(contador);
+
+const idAudio = #som_${classe};
+
+
+    console.log(idAudio);
+    Contador = Contador + 1;
+    console.log(Contador);
 }
