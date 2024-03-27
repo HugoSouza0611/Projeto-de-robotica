5185532413ab3ae67f49d258078b7bfe8d3103f3
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-03-27 11:09:28 +0000

    gfhngjfjgjf

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 alert('Olá, seja bem vindo!!');
 
 let nomeUsuario = prompt ('Qual o seu nome?');
+let texto = document.querySelector('span');
+
+
+
+texto.textContent = nomeUsuario;
+
+while (nome Usuario == ''){
+    nomeUsuario = prompt ('Qual o seu nome?');
+}
