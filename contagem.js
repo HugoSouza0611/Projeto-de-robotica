8474ca4e571c0b426bb85b7a0eaeba115fdbcb7b
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-15 11:28:25 -0300

    asaad

--- contagem.js
+++ contagem.js
@@ -1,15 +1,15 @@
-const baconx = document.querySelectorAll('.botao');
-// baconx.length
-
+const baconx = document.querySelectorAll('.objetivos ');
+const textos = document.querySelectorAll( '.aba-conteudo');
 
 
 
 
 for(let i = 0; i < baconx.length; i++){
-console.log(i);
-baconx[i].onclick = function(){
-    baconx[i].classList.add('ativo');emove('ativo')
-}
-
-//baconx[i].classList.remove('ativo');
+    baconx[i].onclick = function(){
+        for ( let j =0; j < baconx.length; j++)
+        texto [j].classList.romove( 'ativo');
+    textos [ j].classList.remove('ativa');
+    }
+botoes[i].classList.add('ativo');
+textos[i].classList.add ('ativa');
 }
