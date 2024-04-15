6560ea1b4874c8dc608f499357c54caed0a512f9
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-15 12:22:44 -0300

    sdd

--- contagem.js
+++ contagem.js
@@ -14,4 +14,13 @@ baconx[i].classList.add('ativo');
 textos[i].classList.add ('ativa');
 }
   const contadores= document.querySelectorAll('.coontador');
+const tempoObjetivo1= new Date ('2024-04-17T00:00:00');
+let Tempoatual; new Date();
+let segudos= Math.floor (tempoFinal/1000);
+let minutos=Math.floor(segundos/60);
+let hora = Mat.floor(minutos/60;
+let dias= Math.floor (horas/24);
 
+
+
+contadores[0].textContent = dias + horas "horas"+ minutos + "minutos" + segudos + "segundos"
