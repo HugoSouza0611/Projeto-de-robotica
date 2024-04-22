67ca3522cfe983d6409aa91b642ed5284c944ca5
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-22 14:34:43 +0000

    loldldf

--- contagem.js
+++ contagem.js
@@ -1,26 +1,33 @@
 const baconx = document.querySelectorAll('.objetivos ');
-const textos = document.querySelectorAll( '.aba-conteudo');
+const textos = document.querySelectorAll('.aba-conteudo');
 
 
 
 
-for(let i = 0; i < baconx.length; i++){
-    baconx[i].onclick = function(){
-        for ( let j =0; j < baconx.length; j++)
-        baconx[j].classList.romove( 'ativo');
+for (let i = 0; i < baconx.length; i++) {
+  
+  baconx[i].onclick = function () {
+    
+    for (let j = 0; j < baconx.length; j++);
+    baconx[j].classList.romove('ativo');
     textos[j].classList.remove('ativa');
-    }
-baconx[i].classList.add('ativo');
-textos[i].classList.add ('ativa');
+  }
+  baconx[i].classList.add('ativo');
+  textos[i].classList.add('ativa');
 }
-  const contadores= document.querySelectorAll('.coontador');
-const tempoObjetivo1= new Date ('2024-04-30T00:00:00');
+const contadores = document.querySelectorAll('.coontador');
+const tempoObjetivo1 = new Date('2024-04-30T00:00:00');
 let Tempoatual; new Date();
-let segudos= Math.floor (tempoFinal/1000);
-let minutos=Math.floor(segundos/60);
-let hora = Mat.floor(minutos/60;
-let dias= Math.floor (horas/24);
+let segudos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let hora = Mat.floor(minutos / 60;
+let dias = Math.floor(horas / 24);
+
+segudos %= 60;
+minutos %= 60;
+horas %= 24;
 
 
 
-contadores[0].textContent = dias + horas "horas"+ minutos + "minutos" + segudos + "segundos";
+
+contadores[0].textContent = dias + "dias"+ horas + "horas" + minutos;
