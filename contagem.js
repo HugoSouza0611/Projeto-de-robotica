aa54cd09fe5c60e2177bd3431287ad80565d0bed
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-22 14:42:05 +0000

    io

--- contagem.js
+++ contagem.js
@@ -8,13 +8,15 @@ for (let i = 0; i < baconx.length; i++) {
   
   baconx[i].onclick = function () {
     
-    for (let j = 0; j < baconx.length; j++);
+    for (let j = 0; j < baconx.length; j++){
     baconx[j].classList.romove('ativo');
     textos[j].classList.remove('ativa');
   }
   baconx[i].classList.add('ativo');
   textos[i].classList.add('ativa');
 }
+}
+
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date ('2024-04-30T00:00:00');
 
