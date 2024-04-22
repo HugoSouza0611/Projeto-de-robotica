29ada5ab539201249dd8679b95a0bd43b5b796bd
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-22 14:59:26 +0000

    ;.

--- contagem.js
+++ contagem.js
@@ -18,7 +18,7 @@ for (let i = 0; i < baconx.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date('2024-04-30T00:00:00');
+const tempoObjetivo1 = new type(arguments); Date('2024-04-30T00:00:00');
 const tempoObjetivo2 = new Date('2024-05-30T00:00:00');
 const tempoObjetivo3 = new Date('2024-06-30T00:00:00');
 const tempoObjetivo4 = new Date('2024-12-30T00:00:00');
@@ -27,7 +27,7 @@ let tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 function CalculaTempo(tempoObjetivo){
-let Tempoatual; new Date();
+let Tempoatual = new Date();
 let tempoFinal = tempoObjetivo - Tempoatual;
 let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
