707ef06ef2309828a8fadba6dfe7d722eb4727eb
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-04-29 14:13:41 +0000

    ;

--- contagem.js
+++ contagem.js
@@ -9,12 +9,12 @@ for (let i = 0; i < baconx.length; i++) {
   baconx[i].onclick = function () {
     
     for (let j = 0; j < baconx.length; j++){
-    baconx[j].classList.romove('ativo');
+    baconx[j].classList.remove('ativo');
     textos[j].classList.remove('ativa');
   }
+  baconx[i].classList.add('ativo');
+  textos[i].classList.add('ativa');
 }
-baconx[i].classList.add('ativo');
-textos[i].classList.add('ativa');
 }
 
 const contadores = document.querySelectorAll(".contador");
